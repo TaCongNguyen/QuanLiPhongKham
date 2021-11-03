76c7507ba0cb83cb716a1c2679cdbe1d8647af4a
Đạt Nguyễn Thành <18520580@ms.uit.edu.vn>
2021-11-03 21:43:23 +0700

    Add project files.

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class DataProvider_
+    {
+        private string connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+        private static DataProvider_ instance;
+        public static DataProvider_ Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider_();
+                return instance;
+            }
+        }
+        
+        
+
+
+    }
+}
