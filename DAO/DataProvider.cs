02cdca1930013f8bfecc932bc6bbd7fea7baea14
Đạt Nguyễn Thành <18520580@ms.uit.edu.vn>
2021-11-04 16:32:57 +0700

    viet lai dataprovider va BenhNhanDao

--- DAO/DataProvider.cs
+++ DAO/DataProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -23,8 +24,71 @@ namespace DAO
             }
         }
         
-        
+        public DataTable ExecuteQuery(string query,object[] para=null)
+        {
+            DataTable data = new DataTable();
+            try
+            {
+                using(SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
 
+                    // if exist para --> query is stored procedure
+                    // Ex: query="SP_Login @userName,@passWord"
+                    // para = [username,password]
+                    // otherwise it will be a normal query
+                    if (para != null)
+                    {
+                        string[] lstPara = query.Split(
+                            new char[] { ',', ' '},
+                            StringSplitOptions.RemoveEmptyEntries);
+                        int i = 0;
+                        foreach (string item in lstPara)
+                        {
+                            if (item.StartsWith("@"))
+                            {
+                                command.Parameters.AddWithValue(item, para[i]);
+                                i++;
+                            }
+                        }
+                    }
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(data);
+                    connection.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Detail: " + ex.StackTrace);
+            }
+            return data;
+        }
 
+        //ExecuteNonQuery is basically used for operations where there is
+        //nothing returned from the SQL Query or Stored Procedure.
+        //Preferred use will be for INSERT, UPDATE and DELETE Operations.
+        public bool ExecuteNonQuery(string query)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    return true;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+                Console.WriteLine("Detail: " + ex.StackTrace);
+                return false;
+            }
+            
+        }
     }
 }
